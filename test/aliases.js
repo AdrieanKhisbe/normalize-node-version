8d78643b1de85d41740f477729066220427245d9
AdrieanKhisbe <adriean.khisbe@live.fr>
2020-04-04 20:22:55 +0200

    Applied second round of @ehmicky suggestion on #1

--- test/aliases.js
+++ test/aliases.js
@@ -1,45 +1,58 @@
-import { version } from 'process'
+import { versions, cwd as getCwd, chdir } from 'process'
 
 import test from 'ava'
 
 import { resolveVersionRangeAlias } from '../src/aliases.js'
+import normalizeNodeVersion from '../src/main.js'
 
 const resolveInFolder = (versionRange, folder) =>
-  resolveVersionRangeAlias(versionRange, {
+  normalizeNodeVersion(versionRange, {
     cwd: `${__dirname}/fixtures/${folder}`,
   })
 
-test('Resolve - with node-version pseudo version', async (t) => {
+test.serial('Option cwd defaults to the current directory', async (t) => {
+  const currentCwd = getCwd()
+  chdir(`${__dirname}/fixtures/nvmrc-project`)
+
+  try {
+    const versionRange = await normalizeNodeVersion('.')
+    t.is(versionRange, '12.14.1')
+  } finally {
+    chdir(currentCwd)
+  }
+})
+
+test('Resolve . with node-version pseudo version', async (t) => {
   const versionRange = await resolveInFolder('.', 'node-version-project')
-  t.is(versionRange, 'v12.12', 'nvmrc version not resolved')
+  t.is(versionRange, '12.12.0', 'nvmrc version not resolved')
 })
 
-test('Resolve - with nvmrc pseudo version', async (t) => {
+test('Resolve . with nvmrc pseudo version', async (t) => {
   const versionRange = await resolveInFolder('.', 'nvmrc-project')
-  t.is(versionRange, 'v12.14', 'nvmrc version not resolved')
+  t.is(versionRange, '12.14.1', 'nvmrc version not resolved')
 })
 
-test('Resolve - with nave pseudo version', async (t) => {
+test('Resolve . with nave pseudo version', async (t) => {
   const versionRange = await resolveInFolder('.', 'nave-project')
-  t.is(versionRange, 'v12.16', '.nave version not resolved')
+  t.is(versionRange, '12.16.1', '.nave version not resolved')
 })
 
-test('Resolve - in mixed project pseudo version, .node-version having precedence over .nvmrc', async (t) => {
+test('Resolve . in mixed project pseudo version, .node-version having precedence over .nvmrc', async (t) => {
   const versionRange = await resolveInFolder('.', 'mixed-project')
-  t.is(versionRange, 'v12.12', 'not resolved to .node-version')
+  t.is(versionRange, '12.12.0', 'not resolved to .node-version')
 })
 
-test('Resolve -  default to process.version if no version file found', async (t) => {
-  const versionRange = await resolveVersionRangeAlias('.', {
+test('Resolve . default to process.version if no version file found', async (t) => {
+  const versionRange = await normalizeNodeVersion('.', {
     cwd: `${__dirname}/../src`,
   })
-  t.is(versionRange, version, 'not resolved to .node-version')
+  t.is(versionRange, versions.node, 'not resolved to .node-version')
 })
 
-test('Resolve current node pseudo version', async (t) => {
-  const versionRange = await resolveVersionRangeAlias('_')
+test('Resolve - current node pseudo version', async (t) => {
+  const versionRange = await normalizeNodeVersion('_')
 
-  t.is(versionRange, version, 'current version not resolved correctly')
+  t.is(versionRange, versions.node, 'current version not resolved correctly')
 })
 
 test('Transfer node version that are not aliased [fully resolved]', async (t) => {
